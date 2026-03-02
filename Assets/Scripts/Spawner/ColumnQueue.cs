721b490e5805f68297c584cedf62d9bf1362eab7
Brainfart Studio <mitchell@mitchellopitz.net>
2026-03-02 12:51:58 -0600

    refactor: consolidate column spawning and decision maker systems

--- Assets/Scripts/Spawner/ColumnQueue.cs
+++ Assets/Scripts/Spawner/ColumnQueue.cs
@@ -1,32 +1,29 @@
 using UnityEngine;
 using System.Collections.Generic;
-
 public sealed class ColumnQueue : MonoBehaviour
 {
-    [SerializeField] _DummyDecisionMaker decisionMaker;
+    [SerializeField] DecisionMaker decisionMaker;
     [SerializeField] int prewarmColumns = 5;
-
     readonly Queue<ColumnData> queue = new();
-
     void Start()
     {
         for (int i = 0; i < prewarmColumns; i++)
-            queue.Enqueue(decisionMaker.GenerateNextColumn());
+            decisionMaker.EnqueueNext(this);
     }
-
+    public void Enqueue(ColumnData data)
+    {
+        queue.Enqueue(data);
+    }
     public ColumnData GetNext()
     {
         if (queue.Count < 3)
-            queue.Enqueue(decisionMaker.GenerateNextColumn());
-
+            decisionMaker.EnqueueNext(this);
         return queue.Dequeue();
     }
-
     public ColumnData PeekNext()
     {
         if (queue.Count == 0)
-            queue.Enqueue(decisionMaker.GenerateNextColumn());
-
+            decisionMaker.EnqueueNext(this);
         return queue.Peek();
     }
 }
